c7be20838d791d247a62b15919fb95e446b6ec43
ellioth <elliot.houdjal@gmail.com>
2024-05-31 23:32:51 +0200

    Final

--- backSubgoal/admin.py
+++ backSubgoal/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from backSubgoal.models import Subgoal
+
+class SubgoalAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Subgoal, SubgoalAdmin)
